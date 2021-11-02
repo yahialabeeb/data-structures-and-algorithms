e8e81ac5ea37bf05e5e6fe517fbdca36fa8704a4
Yahialabeeb <yahia.labeeb@gmail.com>
2021-11-02 12:49:06 +0200

    resubmit

--- python/code_challenges/array-insert-shift/testtt.py
+++ python/code_challenges/array-insert-shift/testtt.py
@@ -1,13 +1,19 @@
-x= [1,2,3]
-y=23
+x = [42,8,15,23,42]
+y = 16
 
-def tet(x,y):
-    x.append(y)
-    j = len(x)
-    idx = int(j/2)+1
-    for i in reversed(range(idx,j)):
-        x[i],x[i-1]=x[i-1],x[i]
 
-tet(x,y)
-print(x)
+def tet(x, y):
+    d = list(range(len(x)+1))
+    j = len(d)
+    idx = int(j/2)
+    for i in range(j):
+        if i == idx:
+            d[i] = y
+        elif i < idx:
+            d[i] = x[i]
+        else:
+            d[i] = x[i-1]
+    print(d)
 
+
+tet(x, y)
